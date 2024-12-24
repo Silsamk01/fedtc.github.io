111dff8f412a7f4c1252ea90ee5ea9664c07d34c
Silsamk01 <144282506+Silsamk01@users.noreply.github.com>
2024-12-24 13:01:03 -0800

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -8,120 +8,105 @@
 		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 	<link rel="stylesheet" href="style.css">
-	<link rel="icon" href="https://i.ibb.co/zb1QCRC/Screenshot-24.png" type="image/x-icon">
-	<title>Federation Of Data and Technology Council</title>
+	<link rel="icon" href="https://i.ibb.co/YWzxL1h/IMG-0683.jpg" type="image/x-icon">
+	<title>Federation Of Data and Technology Council - FEDTC</title>
 </head>
 
-<body style="background-color: black;">
+<body>
 
-	<div class="nav">
-		<img class="logo" src="https://i.ibb.co/zb1QCRC/Screenshot-24.png" alt="" border="0">
-		<ul class="nav justify-content-end">
-			<li class="nav-item">
-				<a class="nav-link active" aria-current="page" href="#" style="color: #150569;">Ho<span
-						style="color: #FF0000;">me</span></a>
-			</li>
-			<div class="dropdown">
-				<a class="btn btn-secondary dropdown-toggle" href="" role="button" data-bs-toggle="dropdown"
-					aria-expanded="false" style="color: #150569;">
-					About <span style="color: #FF0000;">Us</span>
-				</a>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-white">
+		<div class="container-fluid">
+			<img class="logo" src="https://i.ibb.co/YWzxL1h/IMG-0683.jpg" alt="" border="0">
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+				aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="background-color: black;">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav ms-auto">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#home">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="btn btn-secondary dropdown-toggle" href="about.php" role="button" style="color: #150569;">
+							About <span style="color: #FFC857;">Us</span>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#" style="color: #150569;">Our <span
+								style="color: #FFC857;">Publications</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="membership.php" style="color: #150569;">Become<span
+								style="color: #FFC857;"> a Member</span></a>
 
-				<ul class="dropdown-menu">
-					<li><a class="dropdown-item" href="about.html">Who we are</a></li>
-				</ul>.
+					</li>
+				</ul>
 			</div>
-			<li class="nav-item">
-				<a class="nav-link" href="#" style="color: #150569;">Our <span
-						style="color: #FF0000;">Services</span></a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="membership.html" style="color: #150569;">Become<span style="color: #FF0000;">
-						a
-						Member</span></a>
-			</li>
-			<!-- <li class="nav-item">
-				<a class="nav-link" href="#"><span>Shop</span><i
-						style="font-size: 20px;margin-right: 80px;margin-bottom: 40px;" class="bi bi-shop"></i></a>
-			</li> -->
-
-		</ul>
-		
-	</div>
-
+		</div>
+	</nav>
 	
 
 
-		<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-			<div class="carousel-inner" style="border-radius: 60px;">
-				<div class="carousel-item active">
-					<img height="400px" src="https://i.ibb.co/hC6KWDT/handshake.jpg" alt="handshake"
-						class="d-block w-100">
-					<div id="overlay" class="card-img-overlay"
-						style="margin-left: 40px;color: white;padding-top: 40px;font-weight: bolder;">
-						<h5 id="title" class="card-title">Welcome to the Federation of Data and Technology Counsel</h5>
-						<p id="text" class="card-text" style="font-size: 50px;">Empower, Innovate, Connect <br> .</p>
-						<p class="card-text">Your Journey to Data Excellence Begins Here</p>
-						<!-- Button to open the form -->
-					</div>
 
-				</div>
-				<div class="carousel-item">
-					<img height="400px" src="https://i.ibb.co/0y6cscb/house1.jpg" class="d-block w-100" alt="...">
-					<div id="onelay" class="card-img-overlay"
-						style="margin-left: 40px;color: white;padding-top: 40px;font-weight: bolder;">
-						<h5 id="onetitle" class="card-title">Join the Leaders of Data and Technology</h5>
-						<p id="onetext" class="card-text" style="font-size: 50px;">Collaborate, Lead, Innovate<br></p>
-						<p class="card-text">Transforming Data Into Actionable Insights for a Better Future</p>
-					</div>
-
-				</div>
-
-				<div class="carousel-item">
-					<img height="400px" src="https://i.ibb.co/LPRJmSX/house4.jpg" class="d-block w-100" alt="...">
-					<div id="twolays" class="card-img-overlay"
-						style="margin-left: 40px;color: white;padding-top: 40px;font-weight: bolder;">
-						<h5 id="twotitle" class="card-title">Empowering the Next Generation of Data Experts</h5>
-						<p id="twotext" class="card-text" style="font-size: 50px;">Learn, Grow, Succeed<br></p>
-						<p class="card-text">Your Path to Becoming a Data and Technology Leader Starts Here</p>
-					</div>
-
+	<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+		<div class="carousel-inner" style="border-radius: 60px;">
+			<div class="carousel-item active">
+				<img height="400px" src="https://i.ibb.co/hC6KWDT/handshake.jpg" alt="handshake" class="d-block w-100">
+				<div id="overlay" class="card-img-overlay"
+					style="margin-left: 40px;color: white;padding-top: 40px;font-weight: bolder;">
+					<h5 id="title" class="card-title">Welcome to the Federation of Data and Technology Council</h5>
+					<p id="text" class="card-text" style="font-size: 50px;">Empower, Innovate, Connect <br> .</p>
+					<p class="card-text">Your Journey to Data Excellence Begins Here</p>
+					<!-- Button to open the form -->
 				</div>
 
 			</div>
+			<div class="carousel-item">
+				<img height="400px" src="https://i.ibb.co/YWzxL1h/IMG-0683.jpg" class="d-block w-100" alt="...">
+				<div id="onelay" class="card-img-overlay"
+					style="margin-left: 40px;color: white;padding-top: 40px;font-weight: bolder;">
+					<h5 id="onetitle" class="card-title">Join the Leaders of Data and Technology</h5>
+					<p id="onetext" class="card-text" style="font-size: 50px;">Collaborate, Lead, Innovate<br></p>
+					<p class="card-text">Transforming Data Into Actionable Insights for a Better Future</p>
+				</div>
+
+			</div>
+
+			<div class="carousel-item">
+				<img height="400px" src="https://i.ibb.co/LPRJmSX/house4.jpg" class="d-block w-100" alt="...">
+				<div id="twolays" class="card-img-overlay"
+					style="margin-left: 40px;color: white;padding-top: 40px;font-weight: bolder;">
+					<h5 id="twotitle" class="card-title">Empowering the Next Generation of Data Experts</h5>
+					<p id="twotext" class="card-text" style="font-size: 50px;">Learn, Grow, Succeed<br></p>
+					<p class="card-text">Your Path to Becoming a Data and Technology Leader Starts Here</p>
+				</div>
+
+			</div>
+
 		</div>
+	</div>
 
 	</div>
 	<br>
 	<section id="counter" class="section">
-		<div>
+		<div style="margin-left: 40px;">
 			<span id="counter-number">0</span>+
 			<p>Active Web Visitors</p>
 
 		</div>
 		<div>
-			<span id="counter-number">0</span>Active Web Visitors
+			<span style="  font-size: 2rem;font-weight: bold;color: #007BFF;">200+</span>
+			<p>Active Web Visitors</p>
 
 		</div>
 	</section>
 
 
-	<div style="background-image: url('https://i.ibb.co/zb1QCRC/Screenshot-24.png');background-position: center;">
-<!-- 
-		<div class="about-us-container">
-			<div class="about-us-content">
-				<h1>About Us</h1>
-				<p>
-					PWAN SUCCESS is a real estate marketing, development, and management company; a subsidiary of the
-					PWAN (Property World Africa Network) Group. We are dedicated to making the dream of homeownership
-					a reality for individuals and families. With a vision to transform the real estate industry, we
-					focus on providing affordable, quality housing solutions and professional services that cater to
-					the needs of our clients.
-				</p>
-			</div>
-		</div> -->
+	<div style="background-color: #F9F9F9;">
+
 		<div class="values">
-			<div class="card" style="width: 18rem;color: white;background-color: #FF0000;border-radius: 30px;">
+			<div class="card" style="width: 18rem;color: white;background-color: #001F54;border-radius: 30px;">
 				<div class="card-body">
 					<span><i style="font-size: larger;" class="bi bi-eye"></i></span>
 					<h5 class="card-title">Our Vision</h5>
@@ -129,20 +114,20 @@
 						data
 						and technology...
 					</p>
-					<button class="more-info-btn" onclick="toggleMoreInfo()">Learn More</button>
+					<a href=""><button class="more-info-btn">Learn More</button></a>
 
 				</div>
 			</div>
 
 
-			<div class="card" style="width: 18rem;background-color: grey;color: white;border-radius: 30px;">
+			<div class="card" style="width: 18rem;background-color:#FFC857;color: white;border-radius: 30px;">
 				<div class="card-body">
 					<span><i style="font-size: larger;" class="bi bi-flag"></i></span>
 					<h5 class="card-title">Our Mission Statement</h5>
 					<p class="card-text">To unite and inspire Data and IT professionals and researchers worldwide by
 						fostering excellence, integrity, and innovation.
 					</p>
-					<button class="more-info-btn" onclick="toggleMoreInfo()">Learn More</button>
+					<a href=""><button class="more-info-btn">Learn More</button></a>
 
 				</div>
 			</div>
@@ -155,7 +140,7 @@
 						Celebrate and recognize exceptional achievements in data and technology while maintaining a
 						foundation of honesty, transparency, and accountability.
 						Foster Professional Development</p>
-					<button class="more-info-btn" onclick="toggleMoreInfo()">Learn More</button>
+					<a href="about.html"><button class="more-info-btn">Learn More</button></a>
 
 
 				</div>
@@ -170,7 +155,7 @@
 
 
 		<h2 style="display: flex;justify-content: center;align-items: center;font-size: 40px;
-    text-align: center;color: white;">Contact Us</h2>
+    text-align: center;color: #000;">Contact Us</h2>
 		<div class="contact-us">
 			<form id="contactForm">
 				<div class="form-group">
@@ -218,8 +203,8 @@
 
 	<div class="bottom">
 		<div style="width: 500px;margin-left: 7px;">
-			<img class="log" src="https://i.ibb.co/zb1QCRC/Screenshot-24.png"
-				alt="Whats-App-Image-2024-07-26-at-6-53-59-AM" border="0">
+			<img class="log" src="https://i.ibb.co/YWzxL1h/IMG-0683.jpg" alt="Whats-App-Image-2024-07-26-at-6-53-59-AM"
+				border="0">
 			<p>
 			</p>
 		</div>
@@ -235,6 +220,20 @@
 		</div>
 		<div style="width: 300px;">
 			<h4>Quick Menu</h4>
+			<ul style="list-style-type: none;">
+				<li>
+					<a class="nav-link active" aria-current="page" href="#">Home</a>
+				</li>
+				<li>
+					<a class="nav-link" href="#">About Us</a>
+				</li>
+				<li>
+					<a class="nav-link" href="#">Our publication</a>
+				</li>
+				<li>
+					<a class="nav-link" href="#">Become a Member</a>
+				</li>
+			</ul>
 		</div>
 		<div style="width: 300px;text-decoration: none;">
 			<h4>Follow Us</h4>
@@ -272,7 +271,7 @@
 
 
 
-	<div style="background-color: lightskyblue;color: white;height: 50px;">
+	<div class="footer-bottom">
 		© Copyright Federation Of Data and Technology Council . All Rights Reserved.
 	</div>
 
